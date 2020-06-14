9f26158c1b820989c041b269196cd1af41f4977f
Mihai <danfmihai@hotmail.com>
2020-06-13 23:38:49 -0400

    edited

--- main.tf
+++ main.tf
@@ -7,15 +7,15 @@ resource "random_integer" "ip" {
   #}
 }
 
-resource "proxmox_vm_qemu" "test_server" {
-  #count       = 1
+resource "proxmox_vm_qemu" "vm_server" {
+  count       = 2
   name        = "vm-${var.img_type}-tf"
   target_node = "proxmox"
   clone       = "${var.img_type}-cloudinit-template"
   full_clone = false
   cores = 2
   memory  = 2048
-  #boot  = "c"
+  boot  = "c"
   
   lifecycle {
     ignore_changes = [
@@ -24,7 +24,7 @@ resource "proxmox_vm_qemu" "test_server" {
   }
 
   # Cloud Init Settings
-  ipconfig0 = "ip=192.168.102.${random_integer.ip.result}/24,gw=192.168.102.1"
+  ipconfig0 = "ip=192.168.102.20${count.index + 1}/24,gw=192.168.102.1"
   nameserver = "192.168.102.1"
   sshkeys = <<EOF
   ${var.ssh_key}
