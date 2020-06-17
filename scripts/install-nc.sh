4c446f4dfb9e952bda4ce9a553da4abbd6abde02
Mihai <danfmihai@hotmail.com>
2020-06-17 16:10:02 -0400

    edited

--- scripts/install-nc.sh
+++ scripts/install-nc.sh
@@ -165,15 +165,15 @@ echo ""
 # Make sure that NOBODY can access the server without a password
 #mysql -e "UPDATE mysql.user SET Password = PASSWORD('CHANGEME') WHERE User = 'root'"
 # Kill the anonymous users
-mysql -e "DROP USER ''@'localhost'"
+mysql -u root -e "DROP USER ''@'localhost'"
 # Because our hostname varies we'll use some Bash magic here.
-mysql -e "DROP USER ''@'$(hostname)'"
+mysql  -u root -e "DROP USER ''@'$(hostname)'"
 echo "Kill the anonymous users. Done."
 # Kill off the demo database
-mysql -e "DROP DATABASE test"
+mysql  -u root -e "DROP DATABASE test"
 echo "Kill off the demo database. Done."
 # Make our changes take effect
-mysql -e "FLUSH PRIVILEGES"
+mysql  -u root -e "FLUSH PRIVILEGES"
 /usr/sbin/service mysql stop
 ###configure MariaDB
 mv /etc/mysql/my.cnf /etc/mysql/my.cnf.bak
@@ -613,10 +613,9 @@ echo "Press 'y' to issue optimizations."
 echo "---------------------------------"
 echo ""
 sudo -u www-data php /var/www/nextcloud/occ db:add-missing-indices
-sudo -u www-data php /var/www/nextcloud/occ db:convert-filecache-bigint
-printf '%s\n' y
+yes | sudo -u www-data php /var/www/nextcloud/occ db:convert-filecache-bigint
 echo " "
-echo " The document server will be downloaded - please be patient (~ 300MB) ..."
+#echo " The document server will be downloaded - please be patient (~ 300MB) ..."
 echo " " 
 sudo -u www-data php /var/www/nextcloud/occ security:certificates:import /etc/ssl/certs/ssl-cert-snakeoil.pem
 #sudo -u www-data php /var/www/nextcloud/occ app:install documentserver_community
